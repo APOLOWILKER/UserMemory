c9034ad64eb363f1fece8071b6374d275cedcf26
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-05-10 19:07:46 -0300

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import fastify from "fastify";
+
+const app = fastify();
+
+// servidor http
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log("Server running on http://localhost:3333");
+})
+
+
