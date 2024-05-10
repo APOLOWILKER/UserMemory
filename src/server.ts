afd230ebc0e14a92a71b930d31dca08b968f4dea
APOLOWILKER <wilkerapolocarvalho@gmail.com>
2024-05-10 19:35:15 -0300

    chore; install prisma and eslint

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,18 @@
+import { PrismaClient } from '@prisma/client';
 import fastify from "fastify";
 
 const app = fastify();
+const prisma = new PrismaClient();
+
+// rota
+// HTTP Methods: GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS
+app.get('/users', async () => {
+  const users = await prisma.user.findMany()
+
+  return users;
+});
+
+// API RESTfull - padrõe
 
 // servidor http
 app.listen({
